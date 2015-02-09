6c4d68004461aefb4e0a00c7c8ce9ea406347a93
br0r <viktor@silfverstrom.com>
2015-02-09 14:48:32 +0100

    added double sliders

--- src/js/slider.jsx
+++ src/js/slider.jsx
@@ -17,7 +17,12 @@ var Slider = React.createClass({
     name: React.PropTypes.string.isRequired,
     onChange: React.PropTypes.func,
     onDragStart: React.PropTypes.func,
-    onDragStop: React.PropTypes.func
+    onDragStop: React.PropTypes.func,
+    defaultValue: React.PropTypes.oneOfType([
+        React.PropTypes.number,
+        React.PropTypes.arrayOf(React.PropTypes.number)
+    ]),
+    doubleSlider : React.PropTypes.bool
   },
 
   mixins: [Classable],
@@ -29,18 +34,38 @@ var Slider = React.createClass({
       defaultValue: 0,
       min: 0,
       max: 1,
-      dragging: false
+      dragging: false,
+      doubleSlider : false
     };
   },
 
   getInitialState: function() {
-    var value = this.props.value;
-    if (value == null) value = this.props.defaultValue;
-    var percent = value / this.props.max;
-    if (isNaN(percent)) percent = 0;
-    return {
-      value: value,
-      percent: percent
+    if(this.props.doubleSlider) {
+        var values = this.props.values;
+        if(values == null || values.length != 2) {
+            values = [this.props.min, this.props.max];
+        }
+        var percents = values.map(function(num) {
+            var percent = num / this.props.max;
+            if(isNaN(percent)) {
+                percent = 0;
+            }
+            return percent;
+        }.bind(this));
+        return {
+            values : values,
+            percents : percents
+        };
+
+    } else {
+        var value = this.props.value;
+        if (value == null) value = this.props.defaultValue;
+        var percent = value / this.props.max;
+        if (isNaN(percent)) percent = 0;
+        return {
+          value: value,
+          percent: percent
+        }
     }
   },
 
@@ -49,19 +74,65 @@ var Slider = React.createClass({
       this.setValue(nextProps.value);
     }
   },
+  //FIXME - Make so disabled works
+  _renderDraggables : function() {
+      var createDraggable = function(disabled, percent, index) {
+        return <Draggable axis="x" bound="point"
+                  cancel={disabled ? '*' : null}
+                  start={{x: (percent * 100) + '%'}}
+                  onStart={this._onDragStart}
+                  onStop={this._onDragStop}
+                  onDrag={this._onDragUpdate.bind(this, index)}>
+                  <div className="mui-slider-handle" tabIndex={0}></div>
+            </Draggable>
+      }.bind(this);
 
+      if(this.props.doubleSlider) {
+        return [
+            createDraggable(false, this.state.percents[0], 0),
+            createDraggable(false, this.state.percents[1], 1)
+        ];
+      } else {
+        return createDraggable(false, this.state.percent, 0);
+      }
+  },
   render: function() {
     var classes = this.getClasses('mui-input', {
       'mui-error': this.props.error != null
     });
 
     var sliderClasses = this.getClasses('mui-slider', {
-      'mui-slider-zero': this.state.percent == 0,
+      'mui-slider-zero': typeof this.state.percents == "undefined" && this.state.percent == 0,
       'mui-disabled': this.props.disabled
     });
 
-    var percent = this.state.percent;
-    if (percent > 1) percent = 1; else if (percent < 0) percent = 0;
+
+    var lowPercent, highPercent;
+
+    if(this.props.doubleSlider) {
+        lowPercent = this.state.percents[0];
+        if (lowPercent > 1) lowPercent = 1; else if (lowPercent < 0) lowPercent = 0;
+
+        highPercent = this.state.percents[1];
+        if (highPercent > 1) higjPowPercent = 1; else if (highPercent < 0) highPercent = 0;
+    } else {
+        var percent = this.state.percent;
+        if (percent > 1) percent = 1; else if (percent < 0) percent = 0;
+        lowPercent = percent;
+        highPercent = percent;
+    }
+    
+    var draggables = this._renderDraggables();
+
+    var middleSliderFill = null;
+    if(this.props.doubleSlider) {
+        middleSliderFill = (
+            <div className="mui-slider-selection mui-slider-selection-middle"
+              style={{width: ((highPercent - lowPercent) * 100) + '%', left : (lowPercent * 100) + '%'}}>
+              <div className="mui-slider-selection-fill"></div>
+            </div>
+        );
+    }
 
     return (
       <div className={classes} style={this.props.style}>
@@ -71,20 +142,14 @@ var Slider = React.createClass({
         <span className="mui-input-error">{this.props.error}</span>
         <div className={sliderClasses} onClick={this._onClick}>
           <div ref="track" className="mui-slider-track">
-            <Draggable axis="x" bound="point"
-              cancel={this.props.disabled ? '*' : null}
-              start={{x: (percent * 100) + '%'}}
-              onStart={this._onDragStart}
-              onStop={this._onDragStop}
-              onDrag={this._onDragUpdate}>
-              <div className="mui-slider-handle" tabIndex={0}></div>
-            </Draggable>
+            {draggables}
             <div className="mui-slider-selection mui-slider-selection-low"
-              style={{width: (percent * 100) + '%'}}>
+              style={{width: (lowPercent * 100) + '%'}}>
               <div className="mui-slider-selection-fill"></div>
             </div>
+            {middleSliderFill}
             <div className="mui-slider-selection mui-slider-selection-high"
-              style={{width: ((1 - percent) * 100) + '%'}}>
+              style={{width: ((1 - highPercent) * 100) + '%'}}>
               <div className="mui-slider-selection-fill"></div>
             </div>
           </div>
@@ -116,12 +181,24 @@ var Slider = React.createClass({
   },
 
   getPercent: function() {
-    return this.state.percent;
+    if(this.props.doubleSlider) {
+        return this.state.percents;
+    } else {
+        return this.state.percent;
+    }
   },
 
-  setPercent: function (percent) {
+  setPercent: function (percent, index) {
     var value = this._percentToValue(percent);
-    this.setState({value: value, percent: percent});
+    if(this.props.doubleSlider) {
+        var values = this.state.values;
+        values[index] = value;
+        var percents = this.state.percents;
+        percents[index] = percent;
+        this.setState({values : values, percents : percents});
+    } else {
+        this.setState({value: value, percent: percent});
+    }
   },
 
   clearValue: function() {
@@ -152,22 +229,29 @@ var Slider = React.createClass({
     if (this.props.onDragStop) this.props.onDragStop(e, ui);
   },
 
-  _onDragUpdate: function(e, ui) {
+  _onDragUpdate: function(index, e, ui) {
     if (!this.state.dragging) return;
-    if (!this.props.disabled) this._dragX(e, ui.position.left);
+    if (!this.props.disabled) this._dragX(e, ui.position.left, index);
   },
 
-  _dragX: function(e, pos) {
+  _dragX: function(e, pos, index) {
     var max = this.refs.track.getDOMNode().clientWidth;
     if (pos < 0) pos = 0; else if (pos > max) pos = max;
-    this._updateWithChangeEvent(e, pos / max);
+    this._updateWithChangeEvent(e, pos / max, index);
   },
 
-  _updateWithChangeEvent: function(e, percent) {
-    if (this.state.percent === percent) return;
-    this.setPercent(percent);
+  _updateWithChangeEvent: function(e, percent, index) {
+    if (this.state.percent === percent && !this.props.doubleSlider) return;
+    this.setPercent(percent, index);
     var value = this._percentToValue(percent);
-    if (this.props.onChange) this.props.onChange(e, value);
+    var val;
+    if(this.props.doubleSlider) {
+        val = this.state.values;
+        val[index] = value;
+    } else {
+        val = value;
+    }
+    if (this.props.onChange) this.props.onChange(e, val);
   },
 
   _percentToValue: function(percent) {
